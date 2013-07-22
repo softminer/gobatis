ebade1aa25da5a68e8011d4afbbc53e95223733b
softminer <csshi7889@126.com>
2013-07-22 22:30:22 +0800

    defination of interface IDataMapper

--- src/com/softminer/gobatis/IDataMapper.go
+++ src/com/softminer/gobatis/IDataMapper.go
@@ -1,14 +1,15 @@
 package gobatis
 
 import (
- //  "database/sql/driver"
+	//  "database/sql/driver"
+	"reflect"
+	"container/list"
 )
 
 type IDataMapper interface {
-    Delete(statement string,object interface{}) (affectedCount int,err error)
-    Insert(statement string,object interface{}) (resultObject interface{},err error) 
-    Update(statement string,object interface{}) (affectedCount int)
- //   QueryForObject(statement string,objectStruct type) (resultObject interface{},err error)
-    
-} 
-
+	Delete(statement string, object interface{}) (affectedCount int, err error)
+	Insert(statement string, object interface{}) (resultObject interface{}, err error)
+	Update(statement string, object interface{}) (affectedCount int)
+	QueryForObject(statement string, objectStruct reflect.Type,param interface{}) (resultObject interface{}, err error)
+	QueryForList(statement string, objectStruct reflect.Type,param interface{})  (resultList list.List,err error)
+}
